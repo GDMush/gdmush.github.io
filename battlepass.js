f52696643e70bbb97b37872285160f30a1590ac5
GDMush <gavinwenzel100@gmail.com>
2023-05-24 17:19:26 -0500

    deeer

--- battlepass.js
+++ battlepass.js
@@ -1,16 +1,29 @@
-document.getElementById('scroll-left').onclick = function () {
+let scrollInterval;
+
+document.getElementById('scroll-left').onmouseenter = function () {
+  scrollInterval = setInterval(function () {
     document.getElementById('scroll-container').scrollBy({
       top: 0,
-      left: -document.querySelector('.level').offsetWidth,
+      left: -10,  // Adjust speed by changing this value
       behavior: 'smooth'
     });
-  };
-  
-  document.getElementById('scroll-right').onclick = function () {
+  }, 25);  // Adjust speed by changing this value
+};
+
+document.getElementById('scroll-left').onmouseleave = function () {
+  clearInterval(scrollInterval);
+};
+
+document.getElementById('scroll-right').onmouseenter = function () {
+  scrollInterval = setInterval(function () {
     document.getElementById('scroll-container').scrollBy({
       top: 0,
-      left: document.querySelector('.level').offsetWidth,
+      left: 10,  // Adjust speed by changing this value
       behavior: 'smooth'
     });
-  };
-  
+  }, 25);  // Adjust speed by changing this value
+};
+
+document.getElementById('scroll-right').onmouseleave = function () {
+  clearInterval(scrollInterval);
+};
